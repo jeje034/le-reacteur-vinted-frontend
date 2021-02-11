f2d9ceac0274fc811568b679268941721ecce5aa
Jeje S <54934028+jeje034@users.noreply.github.com>
2021-02-11 13:41:39 +0100

    Suppression de warnings liés au useEffect et à une fonction non utilisée.

--- src/containers/Offer.js
+++ src/containers/Offer.js
@@ -8,25 +8,23 @@ const Offer = () => {
     const { id } = useParams();
     const [offer, setOffer] = useState({});
 
-    const fetchData = async () => {
-        try {
-            const response = await axios.get(
-                //"https://le-reacteur-vinted.herokuapp.com/offer" + id
-                "https://lereacteur-vinted-api.herokuapp.com/offer/" + id
-            );
-            console.log("rp :", response.data);
-            setOffer(response.data);
-            setIsDownloading(false);
-        } catch (error) {
-            console.log("An error occured : ", error);
-        }
-    };
-
     useEffect(() => {
-        fetchData();
-    }, []);
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    //"https://le-reacteur-vinted.herokuapp.com/offer" + id
+                    "https://lereacteur-vinted-api.herokuapp.com/offer/" + id
+                );
+                console.log("rp :", response.data);
+                setOffer(response.data);
+                setIsDownloading(false);
+            } catch (error) {
+                console.log("An error occured : ", error);
+            }
+        };
 
-    const getProductDetail = (productDetail) => {};
+        fetchData();
+    }, [id]);
 
     //450 * 600
     return (
